57ea9b61eb540b6287be1f4bd74e842f23de2aaf
Hidenori Tanaka <hidenori091985@yahoo.co.jp>
2018-08-29 22:28:38 +0900

    Initial Commit

---
+++ book_designPattern_todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  book_designPattern_todo
+//
+//  Created by 田中 秀典 on 2018/08/29.
+//  Copyright © 2018年 田中 秀典. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
